ac9a157893324ca3116bb9483f30a49c095a27ef
Aastha Varma <varmamzn@amazon.com>
2026-02-04 10:41:22 +0530

    add some comments

--- cs336_basics/scaled_dot_product_attention.py
+++ cs336_basics/scaled_dot_product_attention.py
@@ -4,14 +4,14 @@ from cs336_basics.softmax import softmax
 
 
 def sdpa(
-    q: torch.Tensor, 
-    k: torch.Tensor,
-    v: torch.Tensor, 
-    mask: torch.Tensor | None
+    q: torch.Tensor, # [batch_size, ..., seq_len, d_k]
+    k: torch.Tensor, # [batch_size, ..., seq_len, d_k]
+    v: torch.Tensor, # [batch_size, ..., seq_len, d_v]
+    mask: torch.Tensor | None  # [seq_len, seq_len]
 ) -> torch.Tensor:
     d_k = k.shape[-1]
     scores = (q @ k.mT) / math.sqrt(d_k)
     if mask is not None:
-        scores = scores.masked_fill(mask == 0, float("-inf")) # mask == 0 -> masked; mask == 1 -> keep
+        scores = scores.masked_fill(mask == 0, float("-inf")) # mask == 0 -> -inf; mask == 1 -> keep
     attention = softmax(scores, -1) @ v
     return attention
